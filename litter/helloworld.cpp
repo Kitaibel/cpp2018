c52c7f4ae2d5f3ef0ca5bf62f71a6d1ae056613d
Kitaibel <lzliuzy@outlook.com>
2018-09-03 16:31:32 +0800

    edit helloworld

--- litter/helloworld.cpp
+++ litter/helloworld.cpp
@@ -2,7 +2,8 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
 	std::cout<<"Hello World!";
 	return 0;
 }
